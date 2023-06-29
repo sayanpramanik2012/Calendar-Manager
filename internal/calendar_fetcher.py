07453af1f910b52ab3c330b21981d34bc96de5ef
SAYAN PRAMANIK <41908711+sayanpramanik2012@users.noreply.github.com>
2023-06-29 15:28:19 +0530

    event fetcher added

--- internal/calendar_fetcher.py
+++ internal/calendar_fetcher.py
@@ -1,10 +1,33 @@
+import datetime as dt
+from googleapiclient.discovery import build
+import os.path
+from google.oauth2.credentials import Credentials
+
+
 def fetch_all_events():
-    # Implement the code to fetch all current events from the calendar
-    # You can use the Google Calendar API or any other calendar API of your choice
-    # Here, we'll just return some sample data
-    events = [
-        {"title": "Event 1", "date": "2023-06-25", "time": "10:00 AM"},
-        {"title": "Event 2", "date": "2023-06-26", "time": "2:30 PM"},
-        {"title": "Event 3", "date": "2023-06-27", "time": "5:00 PM"},
-    ]
+    if os.path.exists("./assets/token.json"):
+        creds = Credentials.from_authorized_user_file("./assets/token.json")
+    service = build("calendar", "v3", credentials=creds)
+    now = dt.datetime.utcnow().isoformat() + "Z"
+    event_results = (
+        service.events()
+        .list(
+            calendarId="primary",
+            timeMin=now,
+            maxResults=12,
+            singleEvents=True,
+            orderBy="startTime",
+        )
+        .execute()
+    )
+    events = event_results.get("items", [])
+
+    if not events:
+        print("No upcoming events found.")
+        return []
+
+    for event in events:
+        start = event["start"].get("dateTime", event["start"].get("date"))
+        print(start, event["summary"])
+
     return events
