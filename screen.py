85ad5eec525dcc1fbd3fa49dc5de89ba87299c81
SAYAN PRAMANIK <41908711+sayanpramanik2012@users.noreply.github.com>
2023-07-02 02:29:25 +0530

    screen and event picker merged

--- screen.py
+++ screen.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import messagebox
 import json
 from internal import calendar_fetcher
-from event_picker import create_event_window
 
 
 def add_event():
@@ -28,49 +27,66 @@ def add_event():
     time_entry.delete(0, tk.END)
 
 
-def launch_application():
-    # Create the main application window
-    window = tk.Tk()
-    window.title("Calendar Event Manager")
+def update_checkbox_state(data, checkbox_vars, index):
+    # Update the alert value based on checkbox state
+    if checkbox_vars[index].get() == 1:
+        data["dates"][index]["alert"] = "1"
+    else:
+        data["dates"][index]["alert"] = "0"
 
-    # Create a label
-    label = tk.Label(window, text="Add Event to Calendar")
-    label.pack()
 
-    # Create a button to fetch events
-    fetch_button = tk.Button(window, text="Fetch Events", command=fetch_events)
-    fetch_button.pack()
+def update_all_states(data, checkbox_vars):
+    for index in range(len(checkbox_vars)):
+        update_checkbox_state(data, checkbox_vars, index)
 
-    # Create an entry field for the event title
-    global title_entry
-    title_label = tk.Label(window, text="Event Title:")
-    title_label.pack()
-    title_entry = tk.Entry(window)
-    title_entry.pack()
 
-    # Create an entry field for the event date
-    global date_entry
-    date_label = tk.Label(window, text="Event Date:")
-    date_label.pack()
-    date_entry = tk.Entry(window)
-    date_entry.pack()
+def create_checkbox(data, checkbox_vars, index):
+    checkbox = tk.Checkbutton(
+        window,
+        variable=checkbox_vars[index],
+        command=lambda index=index: update_checkbox_state(data, checkbox_vars, index),
+    )
+    checkbox.select() if data["dates"][index]["alert"] == "1" else checkbox.deselect()
+    checkbox.grid(row=index + 1, column=2)
 
-    # Create an entry field for the event time
-    global time_entry
-    time_label = tk.Label(window, text="Event Time:")
-    time_label.pack()
-    time_entry = tk.Entry(window)
-    time_entry.pack()
 
-    # Create a button to add the event
-    add_button = tk.Button(window, text="Add Event", command=add_event)
-    add_button.pack()
+def create_label(date, index):
+    label = tk.Label(window, text=date)
+    label.grid(row=index + 1, column=1)
 
-    event_button = tk.Button(window, text="Show all Event", command=create_event_window)
-    event_button.pack()
 
-    # Start the main event loop
-    window.mainloop()
+def done(data, checkbox_vars):
+    # Update all checkbox states in data before writing to the file
+    update_all_states(data, checkbox_vars)
+    # Update the JSON file with the modified data
+    with open("assets/upcoming.json", "w") as file:
+        json.dump(data, file, indent=2)
+
+
+def create_event_window():
+    # Read the JSON file
+    with open("assets/upcoming.json") as file:
+        data = json.load(file)
+
+    checkbox_vars = []
+
+    # Create checkboxes and labels dynamically
+    for index, date_entry in enumerate(data["dates"]):
+        date = date_entry["date"]
+        alert = date_entry["alert"]
+
+        checkbox_var = tk.IntVar(value=int(alert))
+        checkbox_vars.append(checkbox_var)
+
+        create_label(date, index)
+        create_checkbox(data, checkbox_vars, index)
+
+    done_button = tk.Button(
+        window,
+        text="Done",
+        command=lambda: done(data, checkbox_vars),
+    )
+    done_button.grid(row=len(data["dates"]) + 1, column=1, columnspan=2)
 
 
 def fetch_events():
@@ -84,3 +100,48 @@ def fetch_events():
     messagebox.showinfo(
         "Events Fetched", "All current events fetched and saved in events.json."
     )
+
+
+def launch_application():
+    global window
+    window = tk.Tk()
+    window.title("Calendar Event Manager")
+
+    # Create a label
+    label = tk.Label(window, text="Add Event to Calendar")
+    label.grid(row=0, column=0, columnspan=2)
+
+    # Create a button to fetch events
+    fetch_button = tk.Button(window, text="Fetch Events", command=fetch_events)
+    fetch_button.grid(row=1, column=0, columnspan=2)
+
+    # Create an entry field for the event title
+    global title_entry
+    title_label = tk.Label(window, text="Event Title:")
+    title_label.grid(row=2, column=0, sticky="e")
+    title_entry = tk.Entry(window)
+    title_entry.grid(row=2, column=1, sticky="w")
+
+    # Create an entry field for the event date
+    global date_entry
+    date_label = tk.Label(window, text="Event Date:")
+    date_label.grid(row=3, column=0, sticky="e")
+    date_entry = tk.Entry(window)
+    date_entry.grid(row=3, column=1, sticky="w")
+
+    # Create an entry field for the event time
+    global time_entry
+    time_label = tk.Label(window, text="Event Time:")
+    time_label.grid(row=4, column=0, sticky="e")
+    time_entry = tk.Entry(window)
+    time_entry.grid(row=4, column=1, sticky="w")
+
+    # Create a button to add the event
+    add_button = tk.Button(window, text="Add Event", command=add_event)
+    add_button.grid(row=5, column=0, columnspan=2)
+
+    event_button = tk.Button(window, text="Show all Event", command=create_event_window)
+    event_button.grid(column=1, columnspan=1, pady=(10, 200), padx=(70, 210))
+
+    # Start the main event loop
+    window.mainloop()
